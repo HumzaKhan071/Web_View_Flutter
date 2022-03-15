d0300891680686a805797efca577490882cfa45c
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2022-03-15 13:27:40 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: WebView(
+        initialUrl: 'https://github.com/HumzaKhan071',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
